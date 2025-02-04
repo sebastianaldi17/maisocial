99965bfe75ff50fc49df3bbb7c3b2f7f44baee56
sebastianaldi17 <sebastian.aldi17@gmail.com>
2025-02-04 22:35:14 +0700

    Add min & max level filters to front page

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -8,11 +8,21 @@ import InfiniteScroll from "react-infinite-scroll-component";
 
 const DEBOUNCE_TIME = 500;
 
+const levels: [string, string][] = [];
+for (let i = 1; i <= 15; i++) {
+  levels.push([i.toString(), i.toString() + ".0"]);
+  if (i >= 7 && i < 15) {
+    levels.push([i.toString() + "+", i.toString() + ".6"]);
+  }
+}
+
 export default function Home() {
   const [search, setSearch] = useState("");
   const [filter, setFilter] = useState("title");
   const [category, setCategory] = useState("");
   const [version, setVersion] = useState("");
+  const [minLevel, setMinLevel] = useState("0");
+  const [maxLevel, setMaxLevel] = useState("15");
   const [lastId, setLastId] = useState("");
 
   const [fetching, setFetching] = useState(false);
@@ -37,6 +47,8 @@ export default function Home() {
       filter === "artist" ? search : "",
       category,
       version,
+      minLevel,
+      maxLevel,
     );
   };
 
@@ -46,10 +58,12 @@ export default function Home() {
     artist: string,
     category: string,
     version: string,
+    minLevel: string = "1.0",
+    maxLevel: string = "15.0",
   ) => {
     try {
       const response = await fetch(
-        `${process.env.NEXT_PUBLIC_API_URL}/songs?nextId=${nextId}&title=${title}&artist=${artist}&category=${category}&version=${version}`,
+        `${process.env.NEXT_PUBLIC_API_URL}/songs?nextId=${nextId}&title=${title}&artist=${artist}&category=${category}&version=${version}&minLevel=${minLevel}&maxLevel=${maxLevel}`,
       );
       if (response.ok) {
         const data = await response.json();
@@ -101,13 +115,18 @@ export default function Home() {
       return (
         <div
           key={difficulty._id}
-          className={`relative w-10 h-10 flex items-center justify-center rounded-lg ${isRemaster ? "border border-purple-500" : ""} ${backgroundColor}`}
+          className={`relative w-12 h-12 flex items-center justify-center rounded-lg ${isRemaster ? "border border-purple-500" : ""} ${backgroundColor}`}
         >
           <span
-            className={`text-md ${isRemaster ? "text-purple-500" : "text-white"}`}
+            className={`text-lg ${isRemaster ? "text-purple-500" : "text-white"}`}
           >
             {difficulty.level}
           </span>
+          <div className="absolute w-10 h-4 bottom-9 left-6 bg-gray-800 rounded-2xl shadow-lg">
+            <p className="text-xs text-white text-center align-middle">
+              {difficulty.internalLevel}
+            </p>
+          </div>
           {
             <img
               src={
@@ -116,7 +135,7 @@ export default function Home() {
                   : "/type-std.png"
               }
               alt="difficulty type icon"
-              className="absolute top-0 left-6 w-7 h-2"
+              className="absolute top-9 left-6 w-12 h-4"
             />
           }
         </div>
@@ -135,7 +154,7 @@ export default function Home() {
       return;
     }
     fetchMoreData(true);
-  }, [category, version]);
+  }, [category, version, minLevel, maxLevel]);
 
   useEffect(() => {
     const debounceFn = setTimeout(() => {
@@ -153,15 +172,15 @@ export default function Home() {
     <div>
       <div className="p-2">
         <div className="flex flex-col md:flex-row gap-4 mb-4 max-w-3xl mx-auto">
-          <div className="flex w-full border rounded-lg overflow-hidden">
+          <div className="flex w-full border border-gray-600 rounded-lg overflow-hidden">
             <input
-              className="flex-1 p-2 border-none outline-none"
+              className="flex-1 p-2 border-none outline-none placeholder-gray-500"
               placeholder="Search..."
               value={search}
               onChange={(e) => setSearch(e.target.value)}
             />
             <select
-              className="p-2 border-l"
+              className="p-2 border-l border-gray-600"
               value={filter}
               onChange={(e) => {
                 setFilter(e.target.value);
@@ -172,9 +191,9 @@ export default function Home() {
             </select>
           </div>
         </div>
-        <div className="flex flex-col md:flex-row gap-4 mb-4 max-w-3xl mx-auto">
+        <div className="flex flex-row flex-wrap gap-4 mb-4 max-w-3xl mx-auto">
           <select
-            className="p-2 border rounded w-full md:w-1/2"
+            className="p-2 border border-gray-600 rounded-lg w-2/5 md:w-1/5"
             value={category}
             onChange={(e) => {
               setCategory(e.target.value);
@@ -190,7 +209,7 @@ export default function Home() {
             })}
           </select>
           <select
-            className="p-2 border rounded w-full md:w-1/2"
+            className="p-2 border border-gray-600 rounded-lg w-2/5 md:w-1/5"
             value={version}
             onChange={(e) => setVersion(e.target.value)}
           >
@@ -199,6 +218,38 @@ export default function Home() {
               return (
                 <option key={version} value={version}>
                   {version}
+                </option>
+              );
+            })}
+          </select>
+          <select
+            className="p-2 border border-gray-600 rounded-lg w-2/5 md:w-1/5"
+            value={minLevel}
+            onChange={(e) => setMinLevel(e.target.value)}
+          >
+            <option value="1.0">Min level</option>
+            {levels.map((item) => {
+              const [label, value] = item;
+              if (value === "1.0") return null;
+              return (
+                <option key={`${label}-min`} value={value}>
+                  {label}
+                </option>
+              );
+            })}
+          </select>
+          <select
+            className="p-2 border border-gray-600 rounded-lg w-2/5 md:w-1/5"
+            value={maxLevel}
+            onChange={(e) => setMaxLevel(e.target.value)}
+          >
+            <option value="15.0">Max level</option>
+            {levels.map((item) => {
+              const [label, value] = item;
+              if (value === "15.0") return null;
+              return (
+                <option key={`${label}-max`} value={value}>
+                  {label}
                 </option>
               );
             })}
@@ -219,7 +270,7 @@ export default function Home() {
             {items.map((item) => (
               <Link
                 href={`/song/${item._id}`}
-                className="select-none flex flex-col p-4 my-4 border rounded-lg shadow flex hover:bg-stone-200"
+                className="select-none flex flex-col p-4 my-4 border border-gray-300 rounded-lg shadow-md flex hover:bg-stone-200"
                 key={item._id + "root"}
               >
                 <div key={item._id} className="flex">
@@ -235,12 +286,12 @@ export default function Home() {
                     <p className="text-gray-500 truncate text-ellipsis">
                       {item.artist}
                     </p>
-                    <div className="mt-1 flex-wrap gap-4 hidden md:flex">
+                    <div className="mt-2 flex-wrap gap-8 hidden md:flex">
                       {difficultiesComponent(item.difficulties)}
                     </div>
                   </div>
                 </div>
-                <div className="mt-2 flex flex-wrap gap-4 md:hidden">
+                <div className="mt-4 flex flex-wrap gap-8 md:hidden">
                   {difficultiesComponent(item.difficulties)}
                 </div>
               </Link>
